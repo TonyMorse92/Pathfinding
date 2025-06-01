d7685fdae88423846326d85de1f80d6f4519df20
TonyMorse92 <anthonymorse92@gmail.com>
2025-06-01 14:47:47 -0400

    Have the colors and the basic update loop. Have to add the actual pathing.

--- path_finder.py
+++ path_finder.py
@@ -13,6 +13,7 @@ target_pos = [8,0]
 black = (0,0,0)
 white = (255,255,255)
 grey = (128,128,128)
+red = (255,0,0)
 rows = 400
 columns = 400
 cell_size = 40
@@ -35,7 +36,23 @@ def main():
     target_cell = pygame.Rect(target_pos[0]*cell_size, target_pos[1]*cell_size, cell_size, cell_size)
     pygame.draw.rect(screen, grey, target_cell)
     pygame.display.update()
+
     time.sleep(2)
+
+
+    target_hit = False
+
+    num_moves = 0
+    while not target_hit:
+        num_moves += 1
+        pygame.draw.rect(screen, red, start_cell)
+        start_cell = pygame.Rect(num_moves*cell_size, start_pos[1]*cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, black, start_cell)
+        pygame.display.update()
+        time.sleep(1)
+        if num_moves == 8:
+            target_hit = True
+
 
 def draw_grid():
     for col in range(columns):
